d8029a6ed85d7afcd2181e9d1f71546725d3bedb
tienlb <tienlb@shinway.net>
2019-04-22 15:19:29 +0700

    0.2

--- src/Providers/AdminServiceProvider.php
+++ src/Providers/AdminServiceProvider.php
@@ -27,9 +27,9 @@ class AdminServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'shudanti');
         $this->publishes([
             __DIR__.'/../resources/views' => base_path('resources/views/shudanti/admin'),
-        ]);
+        ], 'test');
         $this->publishes([
             __DIR__.'/../config/test_admin.php' => config_path('test_admin.php'),
-        ], 'admin.config');
+        ], 'test');
     }
 }
